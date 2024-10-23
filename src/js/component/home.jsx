5105f2672b54d1e56171637aacc8378c5e5191cc
Machado123LV <belenmachadolop@gmail.com>
2024-10-23 19:57:13 +0000

    entregar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar", "cocinar", "aprender react"])
+	const [nuevaTarea, setNuevaTarea] = useState("")
+
+	const agregarTarea = (evento) => {
+		evento.preventDefault()
+		if (evento.key === "Enter") {
+			setListaDeTareas([...listaDeTareas, nuevaTarea])
+			setNuevaTarea("")
+		} else {
+			setListaDeTareas([...listaDeTareas, nuevaTarea])
+			setNuevaTarea("")
+		}
+	}
+
+	const borrarTarea = (evento, index) => {
+		evento.preventDefault()
+		let listaFiltrada = listaDeTareas.filter((task, i) => {
+			return (i != index)
+		})
+		setListaDeTareas(listaFiltrada)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<h1 className="text-center mt-5">Todos</h1>
+			<div className="mx-auto col-6">
+				<input type="text" className="form-control" placeholder="¿Que Necesitas Hacer?"
+					value={nuevaTarea} onChange={(evento) => setNuevaTarea(evento.target.value)}
+				/>
+				<div className="d-flex justify-content-center">
+					<button className="btn btn-outline-success mt-3 mb-3" onClick={(evento) => agregarTarea(evento)}>Agregar Tarea</button>
+				</div>
+				<ul className="list-group">
+
+					{listaDeTareas.map((item, index) => {
+						return (
+							<li className="list-group-item" key={index}>
+								{item}	<i className="fa-solid fa-trash icono-oculto" onClick={(evento) => borrarTarea (evento,index)}></i>
+							</li>
+						)
+					})}
+
+				</ul>
+
+
+			</div>
 		</div>
+
 	);
 };
 
